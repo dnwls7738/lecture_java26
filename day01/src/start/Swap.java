4243c125a5edd20e9b0ff56f639ec56b98489610
user <user@DESKTOP-3F0KVPL>
2026-08-20 14:49:46 +0900

    SWAP.java(변수값 스왑)

---
+++ day01/src/start/Swap.java
@@ -0,0 +1,27 @@
+package start;
+
+import java.util.Scanner;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("a 값 입력 :");
+			int a = sc.nextInt();
+			
+			System.out.print("b 값 입력 :");
+			int b = sc.nextInt();
+			
+			System.out.println("a = "+ a + ","+ "b = " + b);
+			
+			int temp = a;  // temp 변수에 a 입력값 할당
+			a = b;			// 비어있는 a 변수에 b 입력값 할당
+			b = temp;		// 비어있는 b 변수에 temp 값 할당
+			
+			System.out.print( "Swapping 결과 " + "a = "+ a + ","+ "b = " + b);
+		}
+	}
+
+}
